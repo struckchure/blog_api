3ff7c71ffc0d66f928b760824eaa619ea8506c4a
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-04 19:08:37 +0100

    adds likes to post serializer

--- core/serializers.py
+++ core/serializers.py
@@ -6,5 +6,5 @@ from core.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["id", "title", "body", "created_at", "updated_at"]
+        fields = ["id", "title", "body", "likes", "created_at", "updated_at"]
         read_only_fields = ["id", "created_at", "updated_at"]
