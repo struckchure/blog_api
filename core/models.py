15cede45beeb9bcbc8c0d33dd54c7e59504f2e4e
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-06 14:02:08 +0100

    feat(core/models): adds user to posts, likes and comment  model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,16 @@
 from blog_api.utils import BaseModel
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Post(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     body = models.TextField()
     likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
@@ -13,6 +18,17 @@ class Post(BaseModel):
 
 class Like(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.post.title} - {self.created_at}"
+        return f"Like - {self.post.title}"
+
+
+class Comment(BaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    reply = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"Comment - {self.post.title}"
